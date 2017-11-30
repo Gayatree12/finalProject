e89813f6c74ebccf31b2d4cf7f850b0e79d56c7b
Gayatree Tamuli <gt86@njit.edu>
2017-11-30 18:40:26 -0500

    registration form created

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
-<html>
-<head><title>My Webpage</title></head>
-<body>
 
-</body>
+<html>
+ 
+ <head>
+ 	<title>My Webpage</title>
+ </head>
+ 
+ <body>
+  <h1>Final Project - IS601</h1>
+  <h2>Create an account</h2>
+  <form action= "index.php" method= "post" enctype= "multipart/form-data">
+  	<input type="text" placeholder="Username" name="username" required /><br><br>
+  	<input type="email" placeholder="Email" name="email" required /><br><br>
+  	<input type="text" placeholder="Firstname" name="firstname" required /><br><br>
+  	<input type="text" placeholder="Lastname" name="lastmname" required /><br><br>
+  	<input type="password" placeholder="Confirm Password" name="confirmpassword" required /><br><br>
+  	<input type="submit" value="Register">
+  </form>
+ </body>
+
 </html>
