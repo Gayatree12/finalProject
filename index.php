9597a8e6cb2ad70185bad74fb171f30d487aeb41
Gayatree Tamuli <gt86@njit.edu>
2017-11-30 22:29:31 -0500

    sign in added

--- index.php
+++ index.php
@@ -2,20 +2,29 @@
 <html>
  
  <head>
- 	<title>My Webpage</title>
+ 	<title>Application Management</title>
+ 	<link rel="stylesheet" type="text/css" href="styles.css">
  </head>
  
  <body>
   <h1>Final Project - IS601</h1>
+  <h1>Todo Task Management Application</h1>
   <h2>Create an account</h2>
-  <form action= "index.php" method= "post" enctype= "multipart/form-data">
-  	<input type="text" placeholder="Username" name="username" required /><br><br>
-  	<input type="email" placeholder="Email" name="email" required /><br><br>
-  	<input type="text" placeholder="Firstname" name="firstname" required /><br><br>
-  	<input type="text" placeholder="Lastname" name="lastmname" required /><br><br>
-  	<input type="password" placeholder="Confirm Password" name="confirmpassword" required /><br><br>
-  	<input type="submit" value="Register">
-  </form>
+  
+    <form action= "index.php" method= "post" enctype= "multipart/form-data">
+    <div class="input">	
+  	  <input type="text" placeholder="Username" name="username" required /><br><br>
+  	  <input type="email" placeholder="Email" name="email" required /><br><br>
+  	  <input type="text" placeholder="Firstname" name="firstname" required /><br><br>
+  	  <input type="text" placeholder="Lastname" name="lastmname" required /><br><br>
+  	  <input type="password" placeholder="Password" name="password" required /><br><br>
+  	  <input type="password" placeholder="Confirm Password" name="confirm password" required /><br><br>
+  	 </div>
+  	  <input type="submit" value="Register">
+  	
+  	<p>Already registered? <a href="login.php">Sign in</a></p>
+    </form>
+  
  </body>
 
 </html>
