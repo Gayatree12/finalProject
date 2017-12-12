9d276292d0183a076fc1f52a1b6f351e177c6851
Gayatree Tamuli <gt86@njit.edu>
2017-12-12 01:03:13 -0500

    table format

--- core/htmltable/htmlTable.php
+++ core/htmltable/htmlTable.php
@@ -4,13 +4,13 @@
 
  class htmlTable {
     public static function genarateTableFromMultiArray($array) {
-        $tableGen = '<table border="1"cellpadding="10">';
+        $tableGen = '<table border=1>';
         $tableGen .= '<tr>';
-        //this grabs the first element of the array so we can extract the field headings for the table
+        //grabs the first element of the array to extract the field headings for the table
         $fieldHeadings = $array[0];
         $fieldHeadings = get_object_vars($fieldHeadings);
         $fieldHeadings = array_keys($fieldHeadings);
-        //this gets the page being viewed so that the table routes requests to the correct controller
+        //gets the page being viewed 
         $referingPage = $_REQUEST['page'];
         foreach ($fieldHeadings as $heading) {
             $tableGen .= '<th>' . $heading . '</th>';
@@ -31,7 +31,7 @@
         return $tableGen;
     }
     public static function generateTableFromOneRecord($innerArray) {
-        $tableGen = '<table border="1" cellpadding="10"><tr>';
+        $tableGen = '<table border=1><tr>';
         $tableGen .= '<tr>';
         foreach ($innerArray as $innerRow => $value) {
             $tableGen .= '<th>' . $innerRow . '</th>';
