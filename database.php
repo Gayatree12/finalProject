ab248302693762bf5f6974036f9741815cb0cb2b
Gayatree Tamuli <gt86@njit.edu>
2017-12-16 22:46:48 -0500

    modified with final changes

--- database.php
+++ database.php
@@ -1,39 +1,44 @@
 <?php
-//adding a directory using namespace
-namespace database;
-//connecting to the database
-//defining constants
-define('DATABASE','gt86');
-define('USERNAME','gt86');
-define('PASSWORD','RAFRIUUsM');
-define('SERVER','sql2.njit.edu');
+
+//turn on debugging messages
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+//define database credentials
+define('CONNECTION', 'sql2.njit.edu');
+define('DATABASE', 'gt86');
+define('USERNAME', 'gt86');
+define('PASSWORD', 'RAFRIUUsM');
+
 
 class dbConn {
-	//variable to hold connection object
+    //variable to hold connection object
     protected static $db;
-    
+    //private construct-class cannot be instatiated externally
     private function __construct() {
-      
-        try {
-           self::$db = new PDO("mysql:host=" . SERVER . ";dbname=" . DATABASE, USERNAME, PASSWORD);
-           //set PDO error mode to exception
-           self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-           //echo "Successfully connected to the database";
-        }
-        //output error message if connection failed
-        catch(PDOException $e) {
-          echo "Unable to connect to the database:" . '<br>' . $e->getMessage();
-        }
-    }
-    //accessible without instantiation
-    public static function getConnection() {
-   	
-   	if(!self::$db) {
-   		//create new connection object
-   		new dbConn();
-   	}
-   	//return the connection
-   	return self::$db;
-    }
-}
+           
+            try {
+              //assigning PDO object to db variable
+              self::$db = new PDO('mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD);
+              //error reporting and throws exceptions
+              self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+             }
 
+            catch (PDOException $e) {
+              //output error if any
+              echo "Connection couldn't be established" . '<br>' . $e->getMessage();
+             }
+        }
+    
+    //get connection function
+    public static function getConnection() {
+          //if no connection object exists then create one
+          if (!self::$db) {
+            //new connection object
+            new dbConn();
+            }
+        
+          //return connection
+          return self::$db;
+        }
+}
