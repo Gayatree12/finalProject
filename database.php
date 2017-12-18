4e46a17d0d8a7ab04b7cada2080357b1064b9ae1
Gayatree Tamuli <gt86@njit.edu>
2017-12-17 19:43:09 -0500

    comments added

--- database.php
+++ database.php
@@ -4,41 +4,37 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-//define database credentials
-define('CONNECTION', 'sql2.njit.edu');
+//define database credentails; set to constant
 define('DATABASE', 'gt86');
 define('USERNAME', 'gt86');
 define('PASSWORD', 'RAFRIUUsM');
-
+define('CONNECTION', 'sql2.njit.edu');
 
 class dbConn {
     //variable to hold connection object
     protected static $db;
-    //private construct-class cannot be instatiated externally
+    //private construct - class cannot be instatiated externally
     private function __construct() {
-           
-            try {
-              //assigning PDO object to db variable
-              self::$db = new PDO('mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD);
-              //error reporting and throws exceptions
-              self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+           try {
+              // assign PDO object to db variable
+              self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
+              //error reporting;throws exceptions
+              self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
              }
-
-            catch (PDOException $e) {
-              //output error if any
-              echo "Connection couldn't be established" . '<br>' . $e->getMessage();
+           catch (PDOException $e) {
+              //output error
+              echo "Connection couldn't be established: " . $e->getMessage();
              }
         }
     
     //get connection function
     public static function getConnection() {
-          //if no connection object exists then create one
-          if (!self::$db) {
-            //new connection object
-            new dbConn();
+           //if no connection object exists then create one
+            if (!self::$db) {
+                //new connection object
+                 new dbConn();
             }
-        
-          //return connection
+          //return connection.
           return self::$db;
         }
 }
