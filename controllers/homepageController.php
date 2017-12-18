b7a84a2510bde7a4eed9a021b733bf07ca8a6570
Gayatree Tamuli <gt86@njit.edu>
2017-12-17 23:55:06 -0500

    pushiing issues

--- controllers/homepageController.php
+++ controllers/homepageController.php
@@ -7,7 +7,7 @@
         $templateData['site_name'] = 'Todo Task Management Application';
 
 		session_start();
-		$templateData['userID'] = @$_SESSION["userID"];
+		$templateData['userID'] = @$_SESSION["userID"];//running the session
 		if($templateData['userID']) {
 			header("Location: index.php?page=accounts&action=all");
 		}		
