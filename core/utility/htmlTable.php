d693e0b537693794468f7a5f7a0889d5fb7dee2b
Gayatree Tamuli <gt86@njit.edu>
2017-12-17 23:20:41 -0500

    html table added

---
+++ core/utility/htmlTable.php
@@ -0,0 +1,56 @@
+<?php
+
+  namespace utility;
+  
+  class htmlTable {
+    public static function genarateTableFromMultiArray($array) {
+
+        $tableGen = '<table border="2"cellpadding="8">';
+        $tableGen .= '<tr>';
+        //gets the first element of the array to extract the field headings for the table
+        $fieldHeadings = $array[0];
+        $fieldHeadings = get_object_vars($fieldHeadings);
+        $fieldHeadings = array_keys($fieldHeadings);
+        $referingPage = $_REQUEST['page'];
+        foreach ($fieldHeadings as $heading) {
+			    if($heading != 'password')
+            	$tableGen .= '<th>' . $heading . '</th>';
+        }
+        $tableGen .= '</tr>';
+        foreach ($array as $record) {
+            $tableGen .= '<tr>';
+            foreach ($record as $key => $value) {
+				if($key == 'id') {
+          $tableGen .= '<td><a href="index.php?page=' . $referingPage . '&action=show&id=' . $value . '">View</a>&nbsp;<a href="index.php?page=' . $referingPage . '&action=edit&id=' . $value . '">Edit</a></td>';
+                } elseif($key == 'isdone') {
+                    $tableGen .= '<td>' . ($value?'Yes':'No').'</td>';
+                } elseif($key != 'password') {
+                    $tableGen .= '<td>' . $value . '</td>';
+                }
+            }
+            $tableGen .= '</tr>';
+        }
+
+        $tableGen .= '</table>';
+
+        return $tableGen;
+    }
+
+    public static function generateTableFromOneRecord($innerArray) {
+        $tableGen = '<table border="2" cellpadding="8"><tr>';
+        $tableGen .= '<tr>';
+        foreach ($innerArray as $innerRow => $value) {
+            $tableGen .= '<th>' . $innerRow . '</th>';
+        }
+        $tableGen .= '</tr>';
+
+        foreach ($innerArray as $value) {
+            $tableGen .= '<td>' . $value . '</td>';
+        }
+
+        $tableGen .= '</tr></table><hr>';
+        return $tableGen;
+    }
+}
+
+?>
