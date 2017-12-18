fe77cfcb640e3db1a1c9a5ca9ba7b6a91e78b89d
Gayatree Tamuli <gt86@njit.edu>
2017-12-17 21:51:14 -0500

    error page direction added

---
+++ pages/error.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Todo Task Management</title>
+    <meta name="description" content="Todo Task Management">
+    <meta name="author" content="SitePoint">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+  <h1><?php echo $data ?></h1>
+
+<script src="js/scripts.js"></script>
+</body>
+</html>
