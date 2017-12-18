8b9a28862a115e6ca0649fac70e5a06df2d761a6
Gayatree Tamuli <gt86@njit.edu>
2017-12-17 21:56:44 -0500

    page not found error added

---
+++ pages/notfound.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>My Task Application</title>
+    <meta name="description" content="Todo Task Management Application">
+    <meta name="author" content="SitePoint">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+<h1>Page Not Found</h1>
+
+<script src="js/scripts.js"></script>
+</body>
+</html>
