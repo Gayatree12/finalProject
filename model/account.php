fe8911837c3ea7f0e136a24d86b0c831e1192834
Gayatree Tamuli <gt86@njit.edu>
2017-12-17 22:51:53 -0500

    changes made with proper indentation

--- model/account.php
+++ model/account.php
@@ -1,6 +1,7 @@
 <?php
 
 final class account extends \database\model {
+    
     public $id;
     public $email;
     public $fname;
@@ -10,34 +11,34 @@ final class account extends \database\model {
     public $gender;
     public $password;
     protected static $modelName = 'account';
+
     public static function getTablename() {
+
         $tableName = 'accounts';
         return $tableName;
     }
-    
-    public static function findOne($id) {
-        
-      $db = dbConn::getConnection();
-      $tableName = get_called_class();
-      $sql = 'SELECT * FROM ' . $tableName . ' WHERE id =' . $id;
-      $statement = $db->prepare($sql);
-      $statement->execute();
-      $class = static::$modelName;
-      $statement->setFetchMode(PDO::FETCH_CLASS, $class);
-      $records =  $statement->fetchAll();
-      return $records;
+    //find users task
+     public static function findTasks() {
 
+        $records = todos::findAll();
+        print_r($records);
+        return $records;
     }
-    
+    //using bcrypt to hash passwords
     public function setPassword($password) {
+
         $password = password_hash($password, PASSWORD_DEFAULT);
+
+
         return $password;
     }
+    //comparing passwords
     public function checkPassword($LoginPassword) {
+
         return password_verify($LoginPassword, $this->password);
     }
-    public function validate()
-    {
+
+    public function validate() {
         $valid = TRUE;
         echo 'myemail: ' . $this->email;
         if($this->email == '') {
